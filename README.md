5ae9274f9cf619a9979da397420d790e6a4d433b
Anh Phan <teoanhss113@gmail.com>
2018-04-17 20:58:15 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Gulp Starter Environment
+
+## Features
+- Browser sync
+- Pug to HTML
+- Sass to CSS
+- Autoprefixer
+- Minify JavaScript
+- Compile JavaScript to ES5 syntax
+- Minify images
+- ...
+
+## Installation
+
+1. Make sure [Node.js](https://nodejs.org) with NPM is installed.
+2. In terminal, clone this repo then `cd` to the folder containing your project.
+3. Continue to use the `npm install` command to download all the dependencies.
+
+## Usage
+
+To use, run the command:
+
+```
+gulp
+```
+
+Use `gulp watch` to watch your files and perform the necessary task when they change.
+
+### Recommended structure for source directory
+
+```
+src
+├── images
+├── js
+├── scss
+└── templates
+```
+
+## License
+
+[MIT](./LICENSE)
---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const plumber = require('gulp-plumber');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+function errorHandler(error) {
+  console.error(error);
+  this.emit('end');
+};
+
+gulp.task('browser-sync', () => {
+  browserSync.init({
+    server: {
+      baseDir: 'dist',
+    },
+  });
+});
+
+gulp.task('clean', () => {
+  return del.sync([
+    'dist/**/*',
+    '!dist/favicon.ico',
+    '!dist/manifest.json'
+  ]);
+});
+
+gulp.task('images', () => {
+  return gulp.src('src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('js', () => {
+  return gulp.src('src/js/**/*.js')
+    .pipe(concat('bundle.js'))
+    .pipe(babel({
+      presets: ['es2015'],
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('pug', () => {
+  return gulp.src('src/*.pug')
+    .pipe(plumber({ errorHandler }))
+    .pipe(pug())
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('sass', () => {
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(plumber({ errorHandler }))
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('watch', ['default'], () => {
+  gulp.watch('src/images/**/*', ['images']);
+  gulp.watch('src/js/**/*.js', ['js']);
+  gulp.watch('src/**/*.pug', ['pug']);
+  gulp.watch('src/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['browser-sync', 'clean', 'images', 'js', 'pug', 'sass']);
